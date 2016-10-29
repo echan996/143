f6df9f06ffb7b1aba13861fe420d63dd8a6d1629
echan996 <echan96@g.ucla.edu>
2016-10-28 20:38:58 -0700

    Tentatively finished

--- newActorDirector.php
+++ newActorDirector.php
@@ -47,40 +47,56 @@
 	</body>
 	<?php
 
-		$db= mysql_connect("localhost", "cs143", "");
-		mysql_select_db("CS143", $db);
+		$db= mysqli_connect("localhost", "cs143", "");
+		if(!$db)
+			die("Unable to connect to database.");
+		if(!mysqli_select_db($db,"CS143"))
+			die("Unable to select DB.");
 		$job=$_GET["job"];
-		$first=mysqli_escape_string(trim($_GET["first"]));
-		$last=mysqli_escape_string(trim($_GET["last"]));
-		$gender=$_GET["gender"];
-		$dob = date_parse(trim($_GET["dob"]));
-		$dod= date_parse(trim($_GET["dod"]));
-		$maxid=mysql_query("SELECT id FROM MaxPersonID",$db);
-		echo $maxid;
-		
+		$first=mysqli_real_escape_string($db, trim($_GET["first"]));
+		$last=mysqli_real_escape_string($db, trim($_GET["last"]));
+		$gender=$_GET["Gender"];
+		$dob =trim($_GET["dob"]);
+		$checkdob = date_parse($dob);
+		$dod = trim($_GET["dod"]);
+		$checkdod= date_parse();
+		$maxid=mysqli_fetch_array(mysqli_query($db,"SELECT id FROM MaxPersonID"))[0];
+		if(!$maxid)
+			echo "Query failed.";
+		$newid=$maxid+1;
+		echo "$newid";
 		if($job=="")
 			echo "You must select Actor or Director.";
 		else if(!preg_match('/^[A-Za-z\.\-\']+$/',$first) || !preg_match('/^[A-Za-z\.\-\']+$/',$last)) //name must be composed of alphabetical char and -,.,' 
 			echo "Invalid first/last name.";
 		else if($gender=="" && $job=="Actor")
 			echo "You must select a gender for Actor.";
-		else if($dob=="" || !checkdate($dob["month"], $dob["day"], $dob["year"]))
+		else if($dob=="" || !checkdate($checkdob["month"], $checkdob["day"], $checkdob["year"]))
 			echo "Invalid date of birth.";
-		else if($dod!="" && !checkdate($dod["month"], $dod["day"], $dod["year"]))
+		else if($dod!="" && !checkdate($checkdod["month"], $checkdod["day"], $checkdod["year"]))
 			echo "Invalid date of death.";
 		else{ // tentative valid input
 			if($job=="Director"){
 				if($dod=="")
-					$tuple="INSERT INTO Director VALUES('$id','$last','$first','$dob',NULL)";
+					$tuple="INSERT INTO Director VALUES('$newid','$last','$first','$dob',NULL)";
 				else
-					$tuple="INSERT INTO Director VALUES('$id','$last','$first','$dob','$dod')";
+					$tuple="INSERT INTO Director VALUES('$newid','$last','$first','$dob','$dod')";
 			}
-			else
+			else{
 				if($dod=="")
-					$tuple="INSERT INTO Director VALUES('$id','$last','$first','$gender','$dob',NULL)";
-				else
-					$tuple="INSERT INTO Director VALUES('$id','$last','$first','$gender','$dob','$dod')";
+					$tuple="INSERT INTO Actor VALUES('$newid','$last','$first','$gender','$dob',NULL)";
+				else{
+					$tuple="INSERT INTO Actor VALUES('$newid','$last','$first','$gender','$dob','$dod')";
+					echo "I'm here!";
+				}
+			}
+			if(mysqli_query($db,$tuple)){
+				//echo ("Error: ".mysqli_error($db));
+				while(!mysqli_query($db,"UPDATE MaxPersonID SET id='$newid' WHERE TRUE" ));
+			}
+			$maxid=mysqli_fetch_array(mysqli_query($db,"SELECT id FROM MaxPersonID"))[0];
+			echo "$maxid";
 		}
-		$result = mysql_query
+		
 	?>
 </html>
